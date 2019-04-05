f6081f7ebd238019c07cf2be0031c6fb8b9430f5
Sephirotha <44975809+Sephirotha@users.noreply.github.com>
2019-04-05 15:37:23 +0100

    Crosshair applied

---
+++ Rejectz_Greed/Assets/u1501269_ONLY_NO_TOUCH/Scripts/LockCursor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCursor : MonoBehaviour
+{
+    public Texture2D crosshairImage;
+    void OnGUI()
+    {
+        float xMin = (Screen.width / 2) - (crosshairImage.width / 2);
+        float yMin = (Screen.height / 2) - (crosshairImage.height / 2);
+        GUI.DrawTexture(new Rect(xMin, yMin, crosshairImage.width, crosshairImage.height), crosshairImage);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
