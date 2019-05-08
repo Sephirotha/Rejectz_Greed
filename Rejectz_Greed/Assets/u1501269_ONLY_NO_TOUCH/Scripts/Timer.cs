7dce2d48499180f206d7c3d8046bb542de1b5636
Sephirotha <44975809+Sephirotha@users.noreply.github.com>
2019-05-08 13:12:01 +0100

    Timer
    I added a timer to the tile puzzle so there is an option to skip every two minutes, also after ten minutes, the player is automatically taken back to Level 2 with the gem.

---
+++ Rejectz_Greed/Assets/u1501269_ONLY_NO_TOUCH/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+[EventHandlerInfoAttribute("Timers",
+                            "Simple Timer",
+                            "Executes the block after an amount of time has elasped")]
+public class Timer : EventHandler
+{
+    public float duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("TimerExpired", duration);
+    }
+
+    void TimerExpired ()
+    {
+        ExecuteBlock();
+            }
+  
+}
